6f7f40c8286e4aa620d69ed4635902589895c46f
ANTOVINI <gabrieldopeitoal@bol.com.br>
2017-05-19 23:50:38 -0300

    Primeminer.sh
    Primeminer.sh

--- primeminer.sh
+++ primeminer.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+#__BEGIN_MAIN__
+#
 echo sudo apt-get install build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y && git clone https://github.com/wolf9466/cpuminer-multi && cd cpuminer-multi/ && chmod +x autogen.sh && ./autogen.sh && CFLAGS="-march=native" ./configure && make && sudo make install && apt-get install screen
+
+#__END_MAIN__
