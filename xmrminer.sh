fd46a4ba607cfe9515b82934fa9a98339dc6e4b2
ANTOVINI <gabrieldopeitoal@bol.com.br>
2017-05-20 09:11:41 -0300

    Xmrminer.sh
    Xmrminer.sh

---
+++ xmrminer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#__BEGIN_MAIN__
+#
+echo sudo apt-get update && wget https://minergate.com/download/deb-cli -O minergate-cli.deb && sudo dpkg -i minergate-cli.deb && apt-get install screen && screen minergate-cli -user antoniovinicioslc@gmail.com -xmr 1
+#__END_MAIN__
