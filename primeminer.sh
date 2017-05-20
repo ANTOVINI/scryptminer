e8155d7879bb5878a9b88ac51a53365244a7ffda
ANTOVINI <gabrieldopeitoal@bol.com.br>
2017-05-19 21:43:05 -0300

    primeminer.sh
    Scrypt dedicada a mineração de cryptomoedas derivados da bitcoin, utilizando a plataforma minergate.

---
+++ primeminer.sh
@@ -0,0 +1,5 @@
+Gdh #!/bin/bash
+#__BEGIN_MAIN__
+#
+echo sudo apt-get install git -y && sudo apt-get install build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y && git clone https://github.com/wolf9466/cpuminer-multi && cd cpuminer-multi/ && chmod +x autogen.sh && ./autogen.sh && CFLAGS="-march=native" ./configure && make && sudo make install && apt-get install screen
+#__END_MAIN__
