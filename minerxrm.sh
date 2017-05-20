2f113a3c6f25724ba582e3f95386bfee7539ddeb
ANTOVINI <gabrieldopeitoal@bol.com.br>
2017-05-19 21:48:41 -0300

    minerxrm.sh
    Scrypt dedicado a minha mineração da cryptomoeda monero.

---
+++ minerxrm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#__BEGIN_MAIN__
+#
+echo sudo apt-get install git -y && sudo apt-get install build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y && git clone https://github.com/wolf9466/cpuminer-multi && cd cpuminer-multi/ && chmod +x autogen.sh && ./autogen.sh && CFLAGS="-march=native" ./configure && make && sudo make install && apt-get install screen && screen minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u antoniovinicioslc@gmail.com -p x
+#__END_MAIN__
