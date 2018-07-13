2e4c9f5038ed42e5ee36efd242f6e38b827c625b
ANTOVINI <gabrieldopeitoal@bol.com.br>
2018-07-13 19:14:01 +0000

    Update ccminer.sh

--- ccminer.sh
+++ ccminer.sh
@@ -3,4 +3,4 @@
 #
 echo git clone https://github.com/tsiv/ccminer-cryptonight.git && cd ccminer-cryptonight && chmod +x autogen.sh && ./autogen.sh && CFLAGS="-march=native" ./configure && make && sudo make install && ./ccminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u antoniovinicioslc@gmail.com -p x
 
-__END_MAIN__
+#__END_MAIN__
