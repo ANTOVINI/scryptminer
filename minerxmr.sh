448bf7b6a0c377927c66b958b4e8ae96d85283b9
ANTOVINI <gabrieldopeitoal@bol.com.br>
2019-04-16 00:45:52 +0000

    Update minerxmr.sh

--- minerxmr.sh
+++ minerxmr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #__BEGIN_MAIN__
 #
-echo sudo apt-get install build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y && git clone https://github.com/wolf9466/cpuminer-multi && cd cpuminer-multi/ && chmod +x autogen.sh && ./autogen.sh && CFLAGS="-march=native" ./configure && make && sudo make install && apt-get install screen && screen minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u antoniovinicioslc@gmail.com -p x
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake -y && sudo apt-get install git -y && git clone https://github.com/hyc/cpuminer-multi && cd cpuminer-multi && ./autogen.sh && CFLAGS="-march=native" && ./configure && make && sudo apt-get install screen && sudo screen ./minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u antoniovinicioslc@gmail.com -p x -t 4
 #__END_MAIN__
